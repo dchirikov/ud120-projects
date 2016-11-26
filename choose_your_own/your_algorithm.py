7db42805ee34664f516dcea6c1128d236f64983f
Dmitry Chirikov <dmitry@chirikov.ru>
2016-11-26 22:58:01 +0100

    Choose your own algorithm

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -17,6 +17,7 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
+"""
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
 plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
@@ -25,19 +26,49 @@ plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
+"""
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.metrics import accuracy_score
+
+from  sklearn.ensemble import RandomForestClassifier as Classifier
+#from  sklearn.ensemble import AdaBoostClassifier as Classifier
+from sklearn.naive_bayes import GaussianNB
+#from  sklearn.cluster import KMeans as Classifier
+
+#clf = Classifier(min_samples_split=2, n_estimators=20, criterion='entropy')
+NB=GaussianNB()
+acc = 0
+
+for e in range(3,101):
+    for r in range(2,101):
+        #rate=(1.0*r/100)
+        #clf = Classifier(n_estimators=e, learning_rate=rate)
+        clf = Classifier(n_estimators=e, min_samples_split=r)
+        rounds = 20
+        r_acc=0
+        for i in range(0,rounds):
+            clf = clf.fit(features_train, labels_train)
+            labels_predict = clf.predict(features_test)
+            r_acc += accuracy_score(labels_test, labels_predict)
+        if r_acc/rounds>acc:
+            acc = r_acc/rounds
+            print(e,r,acc)
+
+clf = Classifier(n_estimators=3, min_samples_split=10)
+clf = clf.fit(features_train, labels_train)
+labels_predict = clf.predict(features_test)
+print(accuracy_score(labels_test, labels_predict))
 
 
 
 
 
-
-
+prettyPicture(clf, features_test, labels_test)
 try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
